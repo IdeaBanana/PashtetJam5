7a0201192c2de543f4fe4570ca89930dcc4cce80
IdeaBanana <pirate228kek@gmail.com>
2024-08-10 15:44:23 +0300

    added list

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -7,9 +7,9 @@ class_name ShipSpawner
 @export var spawnTimer: Timer
 
 @export_group("Sheeps")
-@export var gunBoatSheep: PackedScene =  preload("res://Scenes/GunBoatSheep.tscn")
-@export var sloopSheep: PackedScene = preload("res://Scenes/SloopShip.tscn")
-@export var frigateSheep: PackedScene = preload("res://Scenes/FrigateShip.tscn")
+@export var gunBoatShip: PackedScene =  preload("res://Scenes/GunBoatSheep.tscn")
+@export var sloopShip: PackedScene = preload("res://Scenes/SloopShip.tscn")
+@export var frigateShip: PackedScene = preload("res://Scenes/FrigateShip.tscn")
 var shipArray: Array[PackedScene]
 var currentSceneShips: Array[EnemyShip]
 
@@ -25,30 +25,10 @@ var prevSpawnArray: Array[Marker2D]
 @export var minSpawnTime: float = 0.5
 
 func _ready():
-	initializeSheepArray()
-	spawnTimer.autostart = true
-	spawnTimer.start(randf_range(minSpawnTime, maxSpawnTime))
-	spawnTimer.timeout.connect(_OnSpawnTimerTimeout)
+	pass
 
-func _OnSpawnTimerTimeout():
-	spawnShip()
+func GetRandomShip():
+	pass
 
-func initializeSheepArray(newArray: Array[PackedScene] = []):
-	if newArray and shipArray.size() > 0:
-		shipArray.append_array(newArray)
-	shipArray = [gunBoatSheep, sloopSheep, frigateSheep]
-
-func spawnShip():
-	var spawn := GetRandomSpawn()
-	var shipInstance := GetRandomShip().instantiate()
-	shipInstance.global_position = spawn.global_position
-	add_child(shipInstance)
-
-func GetRandomShip() -> PackedScene:
-	var randomShip: PackedScene = shipArray.pick_random()
-	return randomShip
-
-func GetRandomSpawn() -> Marker2D:
-	var randomSpawn: Marker2D = spawnMarkerParent.get_child(spawnMarkerParent.get_children().find(spawnMarkerParent.get_children().filter(func(child): return child is Marker2D).pick_random()))
-	return randomSpawn
-
+func GetRandomSpawn():
+	pass
